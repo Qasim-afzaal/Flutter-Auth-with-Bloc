2641640a97acc344752c1e8d675a6d44d0ee14ff
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-20 05:10:14 +0500

    add appc ontant

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+class AppConstants {
+  // API Configuration
+  static const String baseUrl = 'https://api.example.com';
+  static const String apiVersion = 'v1';
+  
+  // Storage Keys
+  static const String tokenKey = 'auth_token';
+  static const String userKey = 'user_data';
+  
+  // Timeouts
+  static const int connectionTimeout = 30000; // 30 seconds
+  static const int receiveTimeout = 30000; // 30 seconds
+}
