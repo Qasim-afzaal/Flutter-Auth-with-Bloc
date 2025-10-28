09ceb60324c74c65c8cefd3678fc542bcbe0fa8c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-28 06:18:51 +0500

    fix depandency injection error

--- lib/injection/injection_container.dart
+++ lib/injection/injection_container.dart
@@ -1,8 +1,8 @@
-import 'package:get_it/get_it.dart';
 import '../core/network/api_client.dart';
 import '../features/auth/data/repositories/auth_repository_impl.dart';
 import '../features/auth/domain/repositories/auth_repository.dart';
 import '../features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
 import '../features/counter/presentation/bloc/counter_bloc.dart';
 
 final sl = GetIt.instance;
