f0a0a1349f8ee80e01f16971f9d3da3cec0a755a
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-10-31 05:49:22 +0500

    add states

--- lib/features/counter/presentation/bloc/counter_state.dart
+++ lib/features/counter/presentation/bloc/counter_state.dart
@@ -1,26 +1,18 @@
 import 'package:equatable/equatable.dart';
 
-// States
 abstract class CounterState extends Equatable {
   final int value;
-  
+
   const CounterState(this.value);
-  
-  
+
   @override
   List<Object?> get props => [value];
 }
 
 class CounterInitial extends CounterState {
-  const CounterInitial():super(0);
-  
-  @override
-  String toString() => 'CounterInitial(value: $value)';
+  const CounterInitial() : super(0);
 }
 
 class CounterValueChanged extends CounterState {
-  const CounterValueChanged(super.value);
-  
-  @override
-  String toString() => 'CounterValueChanged(value: $value)';
+  const CounterValueChanged(int value) : super(value);
 }
