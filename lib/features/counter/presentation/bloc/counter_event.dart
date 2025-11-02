68a6f968afddd1ecb0e5e3d349640571e5024e69
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-11-03 04:52:23 +0500

    docs: add documentation to event classes
    - Add comments explaining each event's purpose
    - Improve code documentation for better understanding
    - Complete documentation for all BLoC components

--- lib/features/counter/presentation/bloc/counter_event.dart
+++ lib/features/counter/presentation/bloc/counter_event.dart
@@ -7,16 +7,19 @@ abstract class CounterEvent extends Equatable {
   List<Object?> get props => [];
 }
 
+/// Event to increase the counter value by 1
 class IncreaseNumber extends CounterEvent {
   @override
   String toString() => 'IncreaseNumber';
 }
 
+/// Event to decrease the counter value by 1
 class DecreaseNumber extends CounterEvent {
   @override
   String toString() => 'DecreaseNumber';
 }
 
+/// Event to reset the counter value to 0
 class ResetNumber extends CounterEvent {
   @override
   String toString() => 'ResetNumber';
