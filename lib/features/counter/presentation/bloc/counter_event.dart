f178df05609ea32874059c8ce8b17f9b238374c7
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-11-07 05:56:12 +0500

    Improve documentation in counter_event.dart

--- lib/features/counter/presentation/bloc/counter_event.dart
+++ lib/features/counter/presentation/bloc/counter_event.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
 
+/// Base class for all counter events
+/// All counter-related events must extend this class
 abstract class CounterEvent extends Equatable {
   const CounterEvent();
 
@@ -8,18 +10,24 @@ abstract class CounterEvent extends Equatable {
 }
 
 /// Event to increase the counter value by 1
+/// This event is dispatched when the user taps the increment button
+/// The counter will increase by 1, up to a maximum value of 100
 class IncreaseNumber extends CounterEvent {
   @override
   String toString() => 'IncreaseNumber';
 }
 
 /// Event to decrease the counter value by 1
+/// This event is dispatched when the user taps the decrement button
+/// The counter will decrease by 1, down to a minimum value of -50
 class DecreaseNumber extends CounterEvent {
   @override
   String toString() => 'DecreaseNumber';
 }
 
 /// Event to reset the counter value to 0
+/// This event is dispatched when the user taps the reset button
+/// The counter will be set back to its initial value of 0
 class ResetNumber extends CounterEvent {
   @override
   String toString() => 'ResetNumber';
