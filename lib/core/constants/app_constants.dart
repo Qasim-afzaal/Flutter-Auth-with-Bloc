8258376e2df058b335b846058fd650de9e125161
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-11-12 05:21:52 +0500

    Add animation helper constants and duration helpers

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -31,6 +31,16 @@ class AppConstants {
   static const int mediumAnimationDuration = 300; // milliseconds
   static const int longAnimationDuration = 500; // milliseconds
   
+  // Animation Curves
+  static const String curveEaseIn = 'easeIn';
+  static const String curveEaseOut = 'easeOut';
+  static const String curveEaseInOut = 'easeInOut';
+  
+  // Animation Helpers
+  static Duration shortDuration = const Duration(milliseconds: shortAnimationDuration);
+  static Duration mediumDuration = const Duration(milliseconds: mediumAnimationDuration);
+  static Duration longDuration = const Duration(milliseconds: longAnimationDuration);
+  
   // App Version
   static const String appVersion = '1.0.0';
   static const String appBuildNumber = '1';
