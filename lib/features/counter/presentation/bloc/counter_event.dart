87e223c91a3c3cf68c490b2c9f3d7b0365361ce7
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-11-13 05:57:45 +0500

    Add const constructors and improve toString methods in counter events

--- lib/features/counter/presentation/bloc/counter_event.dart
+++ lib/features/counter/presentation/bloc/counter_event.dart
@@ -13,40 +13,50 @@ abstract class CounterEvent extends Equatable {
 /// This event is dispatched when the user taps the increment button
 /// The counter will increase by 1, up to a maximum value of 100
 class IncreaseNumber extends CounterEvent {
+  const IncreaseNumber();
+  
   @override
-  String toString() => 'IncreaseNumber';
+  String toString() => 'IncreaseNumber()';
 }
 
 /// Event to decrease the counter value by 1
 /// This event is dispatched when the user taps the decrement button
 /// The counter will decrease by 1, down to a minimum value of -50
 class DecreaseNumber extends CounterEvent {
+  const DecreaseNumber();
+  
   @override
-  String toString() => 'DecreaseNumber';
+  String toString() => 'DecreaseNumber()';
 }
 
 /// Event to reset the counter value to 0
 /// This event is dispatched when the user taps the reset button
 /// The counter will be set back to its initial value of 0
 class ResetNumber extends CounterEvent {
+  const ResetNumber();
+  
   @override
-  String toString() => 'ResetNumber';
+  String toString() => 'ResetNumber()';
 }
 
 /// Event to multiply the counter value by 2
 /// This event is dispatched when the user wants to double the current value
 /// The counter will be multiplied by 2, respecting max and min constraints
 class MultiplyNumber extends CounterEvent {
+  const MultiplyNumber();
+  
   @override
-  String toString() => 'MultiplyNumber';
+  String toString() => 'MultiplyNumber()';
 }
 
 /// Event to divide the counter value by 2
 /// This event is dispatched when the user wants to halve the current value
 /// The counter will be divided by 2, respecting max and min constraints
 class DivideNumber extends CounterEvent {
+  const DivideNumber();
+  
   @override
-  String toString() => 'DivideNumber';
+  String toString() => 'DivideNumber()';
 }
 
 /// Event to set the counter to a specific value
