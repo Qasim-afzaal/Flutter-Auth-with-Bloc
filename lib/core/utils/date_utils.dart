870cf096febfecc534636bc243483a52cd87dbba
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-12-03 05:03:35 +0500

    feat: add relative time formatting utility

--- lib/core/utils/date_utils.dart
+++ lib/core/utils/date_utils.dart
@@ -29,5 +29,24 @@ class DateUtils {
   static int getCurrentTimestamp() {
     return DateTime.now().millisecondsSinceEpoch;
   }
+  
+  /// Formats a DateTime to a relative time string
+  /// Returns strings like "2 hours ago", "3 days ago", "just now"
+  static String formatRelativeTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final difference = now.difference(dateTime);
+    
+    if (difference.inSeconds < 60) {
+      return 'just now';
+    } else if (difference.inMinutes < 60) {
+      return '${difference.inMinutes} minute${difference.inMinutes == 1 ? '' : 's'} ago';
+    } else if (difference.inHours < 24) {
+      return '${difference.inHours} hour${difference.inHours == 1 ? '' : 's'} ago';
+    } else if (difference.inDays < 7) {
+      return '${difference.inDays} day${difference.inDays == 1 ? '' : 's'} ago';
+    } else {
+      return formatDate(dateTime);
+    }
+  }
 }
 
